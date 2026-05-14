815f5371efb4718197dee13effba7df1cd6c4b68
potomnazovy <skeleton39876@mail.ru>
2026-05-14 19:50:27 +0300

    complete file

--- vasyakin.kirill/T2/utils.hpp
+++ vasyakin.kirill/T2/utils.hpp
@@ -37,6 +37,27 @@ namespace vasyakin
   {
     std::string& ref;
   };
+
+  struct GuardIO
+  {
+    explicit GuardIO(std::basic_ios< char >& s);
+    ~GuardIO();
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize precision_;
+    std::streamsize width_;
+    std::basic_ios< char >::fmtflags flags_;
+    char fill_;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimeterIO&& dest);
+  std::istream& operator>>(std::istream& in, CharIO&& dest);
+  std::istream& operator>>(std::istream& in, RatioIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
 }
 
 #endif
