b676cac0127b2b4e5a03d71d9b51c5228dd826d6
potomnazovy <skeleton39876@mail.ru>
2026-05-14 19:48:55 +0300

    add structs for my varient and lab

--- vasyakin.kirill/T2/utils.hpp
+++ vasyakin.kirill/T2/utils.hpp
@@ -1,4 +1,42 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+namespace vasyakin
+{
+  struct DataStruct
+  {
+    char key1;
+    std::pair< long long, unsigned long long > key2;
+    std::string key3;
+  };
+
+  struct DelimeterIO
+  {
+    char exp;
+  };
+
+  struct CharIO
+  {
+    char& ref;
+  };
+
+  struct RatioIO
+  {
+    std::pair< long long, unsigned long long >& ref;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  struct LabelIO
+  {
+    std::string& ref;
+  };
+}
 
 #endif
