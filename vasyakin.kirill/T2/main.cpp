e0559f430132989fc4e6665571e801989bcdacf8
potomnazovy <skeleton39876@mail.ru>
2026-05-14 19:57:35 +0300

    complete main.cpp

--- vasyakin.kirill/T2/main.cpp
+++ vasyakin.kirill/T2/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include "utils.hpp"
+
 
 int main()
 {
+  using T = vasyakin::DataStruct;
+  std::vector< T > data;
+  using iit_t = std::istream_iterator< T >;
+
+  std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
+
+  std::sort(data.begin(), data.end());
+
+  using oit_t = std::ostream_iterator< T >;
+  std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
 
   return 0;
 }
