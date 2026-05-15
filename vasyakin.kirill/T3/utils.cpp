243de9ea76938d4f45687a704ca7f2293f6d36b8
potomnazovy <skeleton39876@mail.ru>
2026-05-15 21:59:55 +0300

    add methods for work with triangles area

--- vasyakin.kirill/T3/utils.cpp
+++ vasyakin.kirill/T3/utils.cpp
@@ -16,3 +16,37 @@ vasyakin::GuardIO::~GuardIO()
   s_.flags(flags_);
   s_.fill(fill_);
 }
+
+vasyakin::Triangle vasyakin::make_triangle(const std::vector< Point >& pts, size_t i)
+{
+  return Triangle{pts[0], pts[i], pts[i + 1]};
+}
+
+double vasyakin::triangleArea(Triangle tr)
+{
+  double value1 = static_cast< double >(tr.a.x) * (tr.b.y - tr.c.y);
+  double value2 = static_cast< double >(tr.b.x) * (tr.c.y - tr.a.y);
+  double value3 = static_cast< double >(tr.c.x) * (tr.a.y - tr.b.y);
+
+  return 0.5 * std::fabs(value1 + value2 + value3);
+}
+
+double vasyakin::getArea(const Polygon& poly)
+{
+  if (poly.points.size() < 3)
+  {
+    throw std::invalid_argument("poly size must be >= 3");
+  }
+
+  std::vector< size_t > ind(poly.points.size() - 2);
+  std::iota(ind.begin(), ind.end(), 1);
+
+  std::vector< Triangle > tr(ind.size());
+  std::transform(ind.begin(), ind.end(), tr.begin(),
+    std::bind(make_triangle, std::cref(poly.points), std::placeholders::_1));
+
+  std::vector< double > areas(tr.size());
+  std::transform(tr.begin(), tr.end(), areas.begin(), triangleArea);
+
+  return std::accumulate(areas.begin(), areas.end(), 0.0);
+}
