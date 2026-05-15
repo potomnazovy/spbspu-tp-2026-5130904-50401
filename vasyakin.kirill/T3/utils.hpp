b7f7527031444058ea8ffca540a664a7b34e4ebf
potomnazovy <skeleton39876@mail.ru>
2026-05-15 21:57:26 +0300

    add utils.hpp with namespace

---
+++ vasyakin.kirill/T3/utils.hpp
@@ -0,0 +1,17 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <iterator>
+#include <cstddef>
+#include <numeric>
+#include <functional>
+
+namespace vasyakin
+{
+  
+}
+
+#endif
