09c5b3bf23fb4d22348917e6c4363a61103e2d79
potomnazovy <skeleton39876@mail.ru>
2026-05-16 17:05:38 +0300

    fix cg

--- vasyakin.kirill/T3/main.cpp
+++ vasyakin.kirill/T3/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char* argv[])
     catch (...)
     {
       std::cout << "<INVALID COMMAND>" << '\n';
-      std::cin.clear(); 
+      std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
